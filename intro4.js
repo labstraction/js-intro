db984ffc93920a5782ac57718309ec7332494d00
labstraction <45163263+labstraction@users.noreply.github.com>
2023-03-24 13:09:20 +0100

    capitolo 4 - esercizi

---
+++ intro4.js
@@ -0,0 +1,52 @@
+//ARRAY
+
+//let pippo = [15, 3, 23, 34, 21];
+
+//let pluto = [1, 'ciao', false]  //MALISSIMO
+
+// pippo.push(100); //[15, 3, 23, 34, 21, 100]
+
+// const pluto = pippo.pop() //[15, 3, 23, 34, 21];
+// const paperino = pippo.pop() //[15, 3, 23, 34];
+
+// console.log(pippo); //[15, 3, 23, 34]
+// console.log(pluto); //100
+// console.log(paperino); //21
+
+// pippo.unshift(200); //[200, 15, 3, 23, 34, 21]
+
+// const paperone = pippo.shift(); //[15, 3, 23, 34, 21]
+// const paperina = pippo.shift(); //[3, 23, 34, 21]
+
+// console.log(pippo);//[3, 23, 34, 21]
+// console.log(paperone);//200
+// console.log(paperina);//15
+
+// console.log(pippo);
+// pippo.pop()
+// console.log(pippo); //a volte il log non è affidabile
+
+
+
+const arrayOfStrings = ['andrea', 'jing', 'valentina', 'vlad'];
+
+for (let i = 0; i < arrayOfStrings.length; i++) {
+    const element = arrayOfStrings[i];
+    console.log(element);
+}
+
+
+
+//1) scrivere una funzione che prende come parametro un array di numeri e fa console.log dei numeri con la logica fizzbuzz
+
+
+//2) scrivere una funzione che prende come parametro un array di stringhe e logga quelle più piccole di 5 caratteri
+
+
+//3) scrivere una funzione che prende come parametro una stringa e restituisce una stringa composta dai primi 5 caratteri convertiti in maiuscolo
+
+//4) scrivere una funzione che prende come parametro una stringa e restituisce la lunghezza della stringa senza spazi
+
+//5) scrivere una funzione che prende come parametro una stringa e restituisce il numero degli spazi
+
+
