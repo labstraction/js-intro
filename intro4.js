5732b19ed86755a15af87934066eb140bbe40a69
labstraction <45163263+labstraction@users.noreply.github.com>
2023-03-29 12:55:37 +0200

    tutti i cicli

--- intro4.js
+++ intro4.js
@@ -288,7 +288,63 @@
 // }
 
 // console.log(sumAllElements(numbers)) //867
+//-------------------------------------------------------------------------
+///TUTTI I CICLI IN JAVASCRIPT
 
+const numbers = [9, 8, 7, 1, 2, 3];
+
+//WHILE
+let i = 0
+while (i < numbers.length) {
+    const element = numbers[i];
+    console.log(element);
+    i++;
+}
+//FOR
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    console.log(element);
+}
+//DOWHILE
+let j = 0
+do {
+    const element = numbers[j];
+    console.log(element);
+    j++;
+} while (j<numbers.length);
+//FOROF
+for (const element of numbers) {
+    console.log(element);
+}
+//FOREACH
+numbers.forEach((element) => console.log(element));
+//MAP
+numbers.map((element) => element * 2)
+//FILTER
+numbers.filter((element) => element % 2 === 0)
+//REDUCE
+numbers.reduce((previosElement, currentElement) => previosElement + currentElement, 0);
+
+
+const strings = ['qui', 'quo', 'qua'];
+
+strings.reduce((previosElement, currentElement) => previosElement + currentElement, '');
+
+//FORIN - per ciclare gli oggetti
+
+const andrea = {
+    name: 'andrea',
+    surname: 'asioli',
+    hasADog: true,
+    yob: 1978
+}
+
+for (const key in andrea) {
+    if (Object.hasOwnProperty.call(andrea, key)) {
+        const value = andrea[key];
+        console.log(`andrea ha la proprieta ${key} associata al valore ${value}`)
+    }
+}
 
 //-------------------------------------------------------------------------
 //ESERCIZI
@@ -388,71 +444,155 @@
 
 //1) scrivere una funzione che prende in input un array di numeri è restituisce un array di stringhe in logica FizzBuzz (map)
 
-const numbers = [3, 5, 15, 8, 12, 2];
+// const numbers = [3, 5, 15, 8, 12, 2];
 
-function convertArrayInFizzBuzz(array){
+// function convertArrayInFizzBuzz(array){
+//     const tempArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         let risultato = '';
+//         if (element % 3 === 0 && element % 5 === 0) {
+//             risultato = 'FizzBuzz';
+//         } else if (element % 3 === 0) {
+//             risultato = 'Fizz';
+//         } else if (element % 5 === 0) {
+//             risultato = 'Buzz';
+//         } else {
+//             risultato += element;
+//         }
+//         tempArray.push(risultato);
+//     }
+//     return tempArray;
+// }
 
-}
-
-console.log(convertArrayInFizzBuzz(numbers)) //['Fizz', 'Buzz', 'FizzBuzz', '8', 'Fizz', '2']
+// console.log(convertArrayInFizzBuzz(numbers)) //['Fizz', 'Buzz', 'FizzBuzz', '8', 'Fizz', '2']
 
 //2) scrivere una funzione che prende in input un array di numeri è restituisce un array di numeri in cui i positivi sono trasformati in negativi e viceversa (map)
 
-const numbers1 = [-3, 5, 15, -8, 12, 2, 0];
+// const numbers1 = [-3, 5, 15, -8, 12, 2, 0];
 
-function invertElements(array){
+// function invertElements(array){
+//     const tempArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         let risultato = 0;
+//         if (element !== 0) {
+//             risultato = element * -1;
+//         }
+//         tempArray.push(risultato);
+//     }
+//     return tempArray;
+// }
 
-}
-
-console.log(invertElements(numbers1)) //[3, -5, -15, 8, -12, -2, 0]
+// console.log(invertElements(numbers1)) //[3, -5, -15, 8, -12, -2, 0]
 
 //3) scrivere una funzione che prende in input un array di stringhe è restituisce un array di numeri con la lunghezza delle stringhe (map)
 
-const strings = ['pippo', 'pluto', 'qui'];
+// const strings = ['pippo', 'pluto', 'qui'];
 
-function lengthOfElements(array){
+// function lengthOfElements(array){
+//     const tempArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         tempArray.push(element.length);
+//     }
+//     return tempArray;
+// }
 
-}
-
-console.log(lengthOfElements(strings)) //[4, 5, 3]
+// console.log(lengthOfElements(strings)) //[5, 5, 3]
 
 
 //4) scrivere una funzione che prende in input un array di stringhe è restituisce solo quelle che contengono la lettera z (filter)
 
-const strings2 = ['pippo', 'pluto', 'qui', 'zapotec'];
+// const strings2 = ['pippo', 'pluto', 'qui', 'zapotec']; 
 
-function onlyElementsWithZ(array){
+// function onlyElementsWithZ(array){
+//     const tempArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         // if (element.includes('z')){
+//         //     tempArray.push(element);
+//         // }
+//         if (element.indexOf('z') !== -1){
+//             tempArray.push(element);
+//         } 
+//     } 
+//     return tempArray;
+// }
 
-}
-
-console.log(onlyElementsWithZ(strings2)) //['zapotec']
+// console.log(onlyElementsWithZ(strings2)) //['zapotec']
 
 //5) scrivere una funzione che prende in input un array di numeri e restituisce tutti i numeri pari (filter)
 
-const numbers2 = [-3, 5, 15, -8, 12, 2, 0];
+// const numbers2 = [-3, 5, 15, -8, 12, 2, 0];
 
-function onlyEven(array){
+// function onlyEven(array){
+//     const tempArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         if (element % 2 === 0){
+//             tempArray.push(element);
+//         } 
+//     } 
+//     return tempArray;
+// }
 
-}
-
-console.log(onlyEven(numbers2)) //[8, -12, -2, 0]
+// console.log(onlyEven(numbers2)) //[-8, 12, 2, 0]
 
 //6) scrivere una funzione che prende in input un array di stringhe restituisce una stringa composta dalle iniziali
 //(reduce)
-const strings3 = ['pippo', 'osvaldo', 'paperino'];
+// const strings3 = ['pippo', 'osvaldo', 'paperino'];
 
-function sumFirstChar(array){
+// function sumFirstChar(array){
+//     let tempString = "";
+//         for (let i = 0; i < array.length; i++) {
+//             const element = array[i];
+//             const firstChar = element[0];
+//             tempString += firstChar;
+//         }
+//     return tempString;
+// }
 
-}
-
-console.log(sumFirstChar(strings3)) //'pop'
+// console.log(sumFirstChar(strings3)) //'pop'
 
 //7) scrivere una funzione che prende in input un array di numeri e restituisce il maggiore
 
-const numbers3 = [-3, 5, 15, -8, 12, 2, 0];
+// const numbers3 = [-3, 5, 15, -8, 12, 2, 0];
+// const numbers4 = [-1, -4, -3]
 
-function maxElement(array){
+// function maxElement(array){
+//     let tempNumber = array[0];
+//         for (let i = 1; i < array.length; i++) {
+//             const element = array[i];
+//             if (tempNumber < element) {
+//                 tempNumber = element;
+//             }
+//         }
+//     return tempNumber;
+// }
 
-}
+// function maxElement2(array){
+//     let tempNumber = null;
+//         for (let i = 0; i < array.length; i++) {
+//             const element = array[i];
+//             if (tempNumber === null || tempNumber < element) {
+//                 tempNumber = element;
+//             }
+//         }
+//     return tempNumber;
+// }
 
-console.log(maxElement(numbers3)) //15
+// function maxElement3(array){
+//     let tempNumber = -Infinity;
+//         for (let i = 0; i < array.length; i++) {
+//             const element = array[i];
+//             if (tempNumber < element) {
+//                 tempNumber = element;
+//             }
+//         }
+//     return tempNumber;
+// }
+
+
+// console.log(maxElement(numbers3)) //15
+// console.log(maxElement(numbers4)) //-1
--- intro5.js
+++ intro5.js
@@ -1,224 +1,315 @@
-let a = 5
-let b = "ciao"
+// let a = 5
+// let b = "ciao"
 
-function square(num) {
-    return num*num
-}
+// function square(num) {
+//     return num*num
+// }
 
-let f1 = function(n) {
-    return n*n
-}
+// let f1 = function(n) {
+//     return n*n
+// }
 
-let f2 = (n) => n*n
+// let f2 = (n) => n*n
 
-// Ripetere un'operazione un certo numero di volte
+// // Ripetere un'operazione un certo numero di volte
 
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
 
-function logNumbersFrom0To9() {
-    for (let i = 0; i < 10; i++) {
-        console.log(i);
-    } 
-}
+// function logNumbersFrom0To9() {
+//     for (let i = 0; i < 10; i++) {
+//         console.log(i);
+//     } 
+// }
 
-// Astraggo i dettagli dell'algoritmo all'interno di una funzione
-logNumbersFrom0To9();
+// // Astraggo i dettagli dell'algoritmo all'interno di una funzione
+// logNumbersFrom0To9();
 
 
 
 
-function logNumbersFrom0To(maxNum) {
-    for (let i = 0; i < maxNum; i++) {
-        console.log(i);
-    } 
-}
+// function logNumbersFrom0To(maxNum) {
+//     for (let i = 0; i < maxNum; i++) {
+//         console.log(i);
+//     } 
+// }
 
-function doubleNumbersFrom0To(maxNum) {
-    for (let i = 0; i < maxNum; i++) {
-        console.log(i * 2);
-    } 
-}
+// function doubleNumbersFrom0To(maxNum) {
+//     for (let i = 0; i < maxNum; i++) {
+//         console.log(i * 2);
+//     } 
+// }
 
 
-console.log("\n")
-logNumbersFrom0To(10)
+// console.log("\n")
+// logNumbersFrom0To(10)
 
-console.log("\n")
-logNumbersFrom0To(20)
+// console.log("\n")
+// logNumbersFrom0To(20)
 
 
 
 
 
 
-// Funzioni di ordine superiore
-console.log("FUNZIONI DI ORDINE SUPERIORE: Ripetere una certa operazione a nostra scelta.")
+// // Funzioni di ordine superiore
+// console.log("FUNZIONI DI ORDINE SUPERIORE: Ripetere una certa operazione a nostra scelta.")
 
-function logNumber(n) {
-    console.log(n)
-}
+// function logNumber(n) {
+//     console.log(n)
+// }
 
-function logDouble(n) {
-    console.log(n*2)
-}
+// function logDouble(n) {
+//     console.log(n*2)
+// }
 
-function logHello() {
-    console.log("Hello")
-}
+// function logHello() {
+//     console.log("Hello")
+// }
 
-function repeatFrom0To(maxNum, func) {
-    for (let i = 0; i < maxNum; i++) {
-        func(i) // Che operazione è func? Dipende...
-        // func <- logNumber
-        // func <- logDouble
-        // ...
-    } 
-}
+// function repeatFrom0To(maxNum, func) {
+//     for (let i = 0; i < maxNum; i++) {
+//         func(i) // Che operazione è func? Dipende...
+//         // func <- logNumber
+//         // func <- logDouble
+//         // ...
+//     } 
+// }
 
-console.log("\n")
-repeatFrom0To(10, logNumber)
+// console.log("\n")
+// repeatFrom0To(10, logNumber)
 
-console.log("\n")
-repeatFrom0To(10, logDouble)
+// console.log("\n")
+// repeatFrom0To(10, logDouble)
 
-console.log("\n")
-repeatFrom0To(10, logHello)
+// console.log("\n")
+// repeatFrom0To(10, logHello)
 
-// Chiamare la funzione repeatFrom0To in modo che
-// stampi per ogni numero (i) se i è divisibile per 3  
+// // Chiamare la funzione repeatFrom0To in modo che
+// // stampi per ogni numero (i) se i è divisibile per 3  
 
-function isDivisibleBy3(n) {
-    console.log(n%3 === 0)
-}
+// function isDivisibleBy3(n) {
+//     console.log(n%3 === 0)
+// }
 
-// FUNZIONI ANONIME
+// // FUNZIONI ANONIME
 
-repeatFrom0To(15, function(n) {
-    console.log(n%3 === 0)
-})
+// repeatFrom0To(15, function(n) {
+//     console.log(n%3 === 0)
+// })
 
-// LAMBDA / ARROW FUNCTIONS
+// // LAMBDA / ARROW FUNCTIONS
 
-// setInterval(function() { console.log("Test") }, 1000)
-// setInterval(() => console.log("Test"), 1000);
+// // setInterval(function() { console.log("Test") }, 1000)
+// // setInterval(() => console.log("Test"), 1000);
 
 
 
 
 
 
-console.log("CLOSURE E FUNZIONI CHE RESTITUISCONO ALTRE FUNZIONI")
+// console.log("CLOSURE E FUNZIONI CHE RESTITUISCONO ALTRE FUNZIONI")
 
-// CLOSURE
+// // CLOSURE
 
-let counter = 0
+// let counter = 0
 
-function printAndIncrementCounter() {
-    console.log(counter)
-    counter++
-}
+// function printAndIncrementCounter() {
+//     console.log(counter)
+//     counter++
+// }
 
-printAndIncrementCounter()
-printAndIncrementCounter()
-printAndIncrementCounter()
+// printAndIncrementCounter()
+// printAndIncrementCounter()
+// printAndIncrementCounter()
 
-console.log("Normal console.log: " + counter)
+// console.log("Normal console.log: " + counter)
 
-// FUNZIONI CHE RITORNANO ALTRE FUNZIONI
+// // FUNZIONI CHE RITORNANO ALTRE FUNZIONI
 
-function getRangeCheckFunction(min, max) {
-    return function(n) {
-        return n >= min && n <= max
-    }
-}
+// function getRangeCheckFunction(min, max) {
+//     return function(n) {
+//         return n >= min && n <= max
+//     }
+// }
 
 
-let isBetween10and20 = getRangeCheckFunction(10, 20)
-console.log(isBetween10and20(7))
-console.log(isBetween10and20(1))
-console.log(isBetween10and20(-8))
+// let isBetween10and20 = getRangeCheckFunction(10, 20)
+// console.log(isBetween10and20(7))
+// console.log(isBetween10and20(1))
+// console.log(isBetween10and20(-8))
 
 
 
-function isInRange(n, min, max) {
-    return n >= min && n <= max
-}
+// function isInRange(n, min, max) {
+//     return n >= min && n <= max
+// }
 
-console.log(isInRange(7, 0, 5))
-console.log(isInRange(1, 0, 5))
-console.log(isInRange(-8, 0, 5))
+// console.log(isInRange(7, 0, 5))
+// console.log(isInRange(1, 0, 5))
+// console.log(isInRange(-8, 0, 5))
 
 
-let isBetween0and5 = getRangeCheckFunction(0, 5)
-console.log(isBetween0and5(7))
-console.log(isBetween0and5(1))
-console.log(isBetween0and5(-8))
+// let isBetween0and5 = getRangeCheckFunction(0, 5)
+// console.log(isBetween0and5(7))
+// console.log(isBetween0and5(1))
+// console.log(isBetween0and5(-8))
 
 
 
 
 
-// ARRAY E FUNZIONI DI ORDINE SUPERIORE
-console.log("ARRAY E FUNZIONI DI ORDINE SUPERIORE")
+// // ARRAY E FUNZIONI DI ORDINE SUPERIORE
+// console.log("ARRAY E FUNZIONI DI ORDINE SUPERIORE")
 
-let numbers = [1, 2, 3, 4, 5] // [1, 4, 9, 16, 25]
+// let numbers = [1, 2, 3, 4, 5] // [1, 4, 9, 16, 25]
 
-// map
+// // map
 
-let squaredNumbers = []
+// let squaredNumbers = []
 
-for(let n of numbers) {
-    squaredNumbers.push(n*n)
-}
+// for(let n of numbers) {
+//     squaredNumbers.push(n*n)
+// }
 
-let doubleNumbers = []
+// let doubleNumbers = []
 
-for(let n of numbers) {
-    doubleNumbers.push(n*2)
-}
+// for(let n of numbers) {
+//     doubleNumbers.push(n*2)
+// }
 
-// SCRIVERE UNA FUNZIONE map() CHE DATO IN INGRESSO UN ARRAY,
-// RESTITUISCE UN NUOVO ARRAY CON GLI STESSI ELEMENTI DEL PRIMO
-// TRASFORMATI USANDO UNA FUNZIONE RICEVUTA IN INGRESSO
+// // SCRIVERE UNA FUNZIONE map() CHE DATO IN INGRESSO UN ARRAY,
+// // RESTITUISCE UN NUOVO ARRAY CON GLI STESSI ELEMENTI DEL PRIMO
+// // TRASFORMATI USANDO UNA FUNZIONE RICEVUTA IN INGRESSO
 
-function map(/* ... */) {
-    /* ... */
-}
+// function map(/* ... */) {
+//     /* ... */
+// }
 
-/*
-numbers = [1, 2, 3, 4, 5]
-squaredNumbers = map(numbers, n => n * n)
-// squaredNumbers -> [1, 4, 9, 16, 25]
+// /*
+// numbers = [1, 2, 3, 4, 5]
+// squaredNumbers = map(numbers, n => n * n)
+// // squaredNumbers -> [1, 4, 9, 16, 25]
 
-doubleNumbers = map(numbers, n => n * 2)
-// doubleNumbers -> [2, 4, 6, 8, 10]
-*/
+// doubleNumbers = map(numbers, n => n * 2)
+// // doubleNumbers -> [2, 4, 6, 8, 10]
+// */
 
 
 
 
-// MAP, ecc.
-console.log("MAP, FILTER, ecc.")
+// // MAP, ecc.
+// console.log("MAP, FILTER, ecc.")
 
-let testArray = [9,9,3]
-console.log(testArray.map(n => n * n))
+// let testArray = [9,9,3]
+// console.log(testArray.map(n => n * n))
 
-console.log(testArray.filter(n => n <= 5))
+// console.log(testArray.filter(n => n <= 5))
 
-let sum = 0;
-testArray.forEach(n => sum += n)
-console.log(sum)
+// let sum = 0;
+// testArray.forEach(n => sum += n)
+// console.log(sum)
 
 
 
-let words = ["ciao", "pippo", "topolino", "ciao mondo"]
-let res = words
-            .filter(w => w.length > 5)
-            .map(w => w.length)
-            .forEach(numberOfChars => console.log(numberOfChars))
+// let words = ["ciao", "pippo", "topolino", "ciao mondo"]
+// let res = words
+//             .filter(w => w.length > 5)
+//             .map(w => w.length)
+//             .forEach(numberOfChars => console.log(numberOfChars))
 
-console.log([1,2,3,4,5].reduce((accumulator,num) => accumulator * num, 0))
+// console.log([1,2,3,4,5].reduce((accumulator,num) => accumulator * num, 0))
 
+//---------------------------------------------------------------------
+//Interrogazione
 
+// console.log([3, 5, 12, 1].map((numero) => numero * 2));
+
+
+// function loopNumbersFrom1To15AndApplyAFunction(myStrangeFunction, mySecondStrangeFunction){
+//     for (let i = 1; i < 16; i++) {
+//         const resultOfTheStrangeFunction = myStrangeFunction(i);
+                                        
+//         mySecondStrangeFunction(resultOfTheStrangeFunction)
+
+//     }
+// }
+
+// function divideBy2IfEven(myNumber) {
+//     if (myNumber % 2 === 0) {
+//         return myNumber / 2;
+//     }
+//     return myNumber;
+// }
+
+// function logIfGreaterThan4(myNumber) {
+//     if (myNumber > 4) {
+//         console.log(myNumber);
+//     }
+// }
+
+// loopNumbersFrom1To15AndApplyAFunction(divideBy2IfEven, logIfGreaterThan4);
+
+// loopNumbersFrom1To15AndApplyAFunction(logIfGreaterThan4, divideBy2IfEven);
+
+
+
+// function createMultiplicator(myNumber){
+
+//     function multiply(selectedNumber){
+//         return selectedNumber * myNumber; 
+//     }
+
+//     return multiply
+// }
+
+// function createMultiplicator(myNumber){
+
+//     // function multiply(selectedNumber){
+//     //     return selectedNumber * myNumber; 
+//     // }
+
+//     return (selectedNumber) => selectedNumber * myNumber;
+// }
+
+//const createMultiplicator = (myNumber) => (selectedNumber) => selectedNumber * myNumber;
+
+// function multiply(selectedNumber){
+//     return selectedNumber * 2; 
+// }
+
+// function multiply(selectedNumber){
+//     return selectedNumber * 3; 
+// }
+
+// function multiply(selectedNumber){
+//     return selectedNumber * 5; 
+// }
+
+// const multiplicatorBy2 = createMultiplicator(2);
+// const multiplicatorBy3 = createMultiplicator(3);
+
+// console.log(multiplicatorBy2(5)) //10
+// console.log(multiplicatorBy3(4)) //12
+
+// console.log(createMultiplicator(5)(4)) //20
+
+
+
+// function applyTwice(myFunction){
+//     return (input) => myFunction(myFunction(input));
+// }
+
+// function add1(selectedNumber) {
+//     return selectedNumber + 1;
+// }
+
+// const add2 = applyTwice(add1);
+//             //(input) = add1(add1(input));
+// const add4 = applyTwice(add2);
+
+// console.log(add2(5)) //7
+// console.log(add4(10)) //14
+
+// console.log(applyTwice(applyTwice(add1))(10)) //14
